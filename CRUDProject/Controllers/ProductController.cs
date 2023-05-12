ce446bf0faaea50f1e54418049c5b59b7c774cda
HP <HP@RAINA>
2023-05-12 09:12:32 +0530

    Add project files.

---
+++ CRUDProject/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using CRUDProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductDAL obj = new ProductDAL();
+
+        public ViewResult DisplayProducts()
+        {
+            return View(obj.GetProducts(null));
+        }
+        public ViewResult DisplayProduct(int ProductId)
+        {
+            return View(obj.GetProduct(ProductId, true));
+        }
+        [HttpGet]
+        public ViewResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public RedirectToRouteResult AddProduct(Product product)
+        {
+            product.Status = true;
+            obj.AddProduct(product);
+            return RedirectToAction("DisplayProducts");
+        }
+        public ViewResult EditProduct(int ProductId)
+        {
+
+            Product product1 = obj.GetProduct(ProductId, true);
+            return View(product1);
+        }
+        public RedirectToRouteResult UpdateProduct(Product product)
+        {
+            obj.UpdateProduct(product);
+            return RedirectToAction("DisplayProducts");
+        }
+        public RedirectToRouteResult DeleteProduct(int ProductId)
+        {
+
+            obj.DeleteProduct(ProductId);
+            return RedirectToAction("DisplayProducts");
+        }
+    }
+}
---
+++ CRUDProject/Models/ProductDAL.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace CRUDProject.Models
+{
+    public class ProductDAL
+    {
+        ASPDBDataContext dc;
+        public ProductDAL()
+        {
+            string ConStr = ConfigurationManager.ConnectionStrings["ASPDBConnectionString"].ConnectionString;
+            dc = new ASPDBDataContext();
+        }
+        public List<Product> GetProducts(bool? Status)
+        {
+            List<Product> products;
+            if (Status == null)
+            {
+                products = dc.Products.ToList();
+            }
+            else
+            {
+                products = (from p in dc.Products where p.Status == Status select p).ToList();
+            }
+            return products;
+        }
+        public Product GetProduct(int ProductId, bool? Status)
+        {
+            Product product;
+
+            if (Status == null)
+            {
+                product = (from p in dc.Products where p.ProductId == ProductId select p).Single();
+            }
+            else
+            {
+                product = (from p in dc.Products where p.ProductId == ProductId && p.Status == Status select p).Single();
+               
+            }
+            return product;
+        }
+        public void AddProduct(Product product)
+        {
+            dc.Products.InsertOnSubmit(product);
+            dc.SubmitChanges();
+        }
+        public void UpdateProduct(Product newValues)
+        {
+            Product oldValues = dc.Products.Single(P => P.ProductId == newValues.ProductId);
+            oldValues.ProductName = newValues.ProductName;
+            oldValues.CategoryId = newValues.CategoryId;
+            oldValues.CategoryName = newValues.CategoryName;
+            dc.SubmitChanges();
+        }
+
+
+        public void DeleteProduct(int ProductId)
+        {
+            Product oldValues = dc.Products.First(P => P.ProductId == ProductId);
+            dc.Products.DeleteOnSubmit(oldValues); //Permenant Deletion 
+            //oldValues.Status = false; //Updates the status in Database with-out deleting the record
+            dc.SubmitChanges();
+
+        }
+    }
+}
